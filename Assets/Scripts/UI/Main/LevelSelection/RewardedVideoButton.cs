79ddca02bcda85cb2bc78a45cf4e5fd7b224f725
Senzaj <sanamistik@mail.ru>
2023-07-25 13:15:06 +0300

    Rewarded video blinking

--- Assets/Scripts/UI/Main/LevelSelection/RewardedVideoButton.cs
+++ Assets/Scripts/UI/Main/LevelSelection/RewardedVideoButton.cs
@@ -1,4 +1,5 @@
 using Agava.YandexGames.Samples;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +9,16 @@ public class RewardedVideoButton : MonoBehaviour
 {
     [SerializeField] private Image _videoImage;
     [SerializeField] private Yandex _yandex;
+    [SerializeField] private Color _startColor = Color.white;
+    [SerializeField] private Color _endColor;
+    [SerializeField] private float _blinkingDuration;
 
     private CanvasGroup _canvasGroup;
 
     private void OnEnable()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
+        _videoImage.color = _startColor;
     }
 
     public void Show()
@@ -26,6 +31,7 @@ public class RewardedVideoButton : MonoBehaviour
         _canvasGroup.alpha = 1;
         _canvasGroup.interactable = true;
         _canvasGroup.blocksRaycasts = true;
+        TurnOnFlicker();
     }
 
     public void TurnOff()
@@ -33,5 +39,17 @@ public class RewardedVideoButton : MonoBehaviour
         _canvasGroup.alpha = 0.5f;
         _canvasGroup.interactable = false;
         _canvasGroup.blocksRaycasts = false;
+        TurnOffFlicker();
+    }
+
+    private void TurnOnFlicker()
+    {
+        _videoImage.DOColor(_endColor, _blinkingDuration).SetLoops(-1,LoopType.Yoyo);
+    }
+
+    private void TurnOffFlicker()
+    {
+        _videoImage.DOKill();
+        _videoImage.color = _startColor;
     }
 }
